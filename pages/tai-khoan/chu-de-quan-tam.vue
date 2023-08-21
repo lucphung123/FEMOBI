10a58506337c10938b9a7c2907d2a3b11d962c40
Nguyễn Tâm Thành <thanhh16012k@gmail.com>
2023-08-21 09:48:34 +0700

    update modal

--- pages/tai-khoan/chu-de-quan-tam.vue
+++ pages/tai-khoan/chu-de-quan-tam.vue
@@ -16,8 +16,11 @@
             <p>Bạn chưa quan tâm đến chủ đề nào. Vui lòng thêm chủ đề!</p>
           </div>
           <div class="justify-center flex">
+            <Modal v-show="showModal" @close-modal="showModal = false" />
+
             <button
               class="hover-bg-[#f1b821] hover-text-[#1d1d1b] justify-center mt-5 bg-[#004390] h-10 text-[white] text-sm px-[20px] rounded-[20px] font-bold"
+              @click="showModal = true"
             >
               THÊM CHỦ ĐỀ
             </button>
@@ -29,4 +32,7 @@
 </template>
 <script setup>
 import UserProfile from "../../components/account/UserProfile.vue";
+import Modal from "../../components/modal/Modal.vue";
+
+const showModal = ref(false);
 </script>
