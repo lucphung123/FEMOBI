60428fce2f8e9f597fa5c3653a5791e244093d64
Luc <phungluctnt321@gmai.com>
2023-08-29 11:41:43 +0700

    update

--- pages/khoa-hoc/le.vue
+++ pages/khoa-hoc/le.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex space-x-4 my-5 mt-7 items-center">
+  <div class="flex space-x-4 my-5 mt-7 items-center w-full">
     <nuxt-link to="/" class="text-gray-400">Trang chủ</nuxt-link>
     <div class="i-mdi:chevron-right bg-gray"></div>
     <p class="font-bold text-gray-500">khoá học lẻ</p>
@@ -41,120 +41,146 @@
       </div>
     </div>
   </div>
-  <div class="flex sticky flex-col transition duration-[all] z-5 bg-blue-500 ease-[ease-in-out] w-screen top-23">
-    <div class="lg:(w-225 px-0) xl:(w-320) 2xl:(w-340) mx-auto">
-      <div class="flex items-center">
-        <img src="/img/Hashtag.svg" class="w-30 h-30" alt="" />
-        <div>
-          <p class="text-5 text-color_6 font-bold">hocsinh</p>
-          <div class="flex px-2 rounded-lg bg-color_16 justify-center items-center gap-2 py-1 mt-2">
-            <div class="i-mdi:facebook text-white"></div>
-            <a href="" class="text-white text-sm"> Share</a>
+  <div class="flex flex-col transition duration-[all] z-5 bg-gray-200 ease-[ease-in-out] w-screen my-10">
+    <div class="lg:(w-225 px-0) xl:(w-320) 2xl:(w-340) mx-auto flex space-x-10">
+      <div class="bg-white w-5/7 rd-1 my-15">
+        <div class="p10 space-y-5">
+          <div class="flex bg-#f2f7fd rd-2 sticky top-23 ease-[ease-in-out]">
+            <button
+              v-for="tab in data_tab"
+              :key="tab.id"
+              @click="changeTab(tab)"
+              :class="{
+                'bg-color_4': activeTab === tab,
+                'bg-#f2f7fd text-color_4': activeTab !== tab,
+              }"
+              class="px-4 py-2 rd-2 focus:outline-none text-xl"
+            >
+              {{ tab.name }}
+            </button>
           </div>
+          <div class="text-3xl text-color_4 font-600" id="1">Tổng quan</div>
+          <div class="text-lg">
+            <p v-if="showFullText">{{ longText }}</p>
+            <p v-else>{{ shortenedText }}</p>
+            <button @click="toggleText" class="text-blue-500 bg-white text-lg">
+              {{ showFullText ? "Rút gọn" : "Xem thêm" }}
+            </button>
+          </div>
+          <div class="text-3xl text-color_4 font-600">Lợi ích</div>
+
+          <div>
+            <ul>
+              <li class="cursor-pointer" v-for="(item, index) in menuItems" :key="index">
+                <div class="flex items-center justify-between text-base leading-6 bg-[#E3EFF9] p-5 text-xl" @click="toggleItem(item)">
+                  <div class="flex items-center">
+                    <div class="ml-2">
+                      <NuxtLink :to="item.url">
+                        {{ item.title }}
+                      </NuxtLink>
+                    </div>
+                  </div>
+                  <div v-if="item.children && item.children.length" :class="item.open ? 'i-mdi:chevron-up md:text-2xl' : 'i-mdi:chevron-down md:text-xl'"></div>
+                </div>
+                <div class="overflow-auto transition-all scrollbar-hide" :class="item.open && index === 0 ? 'h-32 duration-500' : 'h-0 duration-500'">
+                  <div v-for="(childItem, childIndex) in item.children" :key="childIndex">
+                    <div class="flex items-center text-base leading-6 p-5 ml-8">
+                      <div :class="item.icon"></div>
+                      <NuxtLink :to="childItem.url">
+                        {{ childItem.title }}
+                      </NuxtLink>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="text-3xl text-color_4 font-600">ĐÁNH GIÁ</div>
+        </div>
+      </div>
+      <div class="w-2/7 my-15 space-y-5 sticky top-23 ease-[ease-in-out]">
+        <div class="text-xl font-semibold text-color_4 sticky top-23 ease-[ease-in-out]"><p>Khoá học phù hợp</p></div>
+        <div class="bg-color_4 text-white rd-2 p-5 sticky top-23 ease-[ease-in-out]">
+          <p>
+            - Học sinh sinh viên cần hoàn thiện các kỹ năng mềm kỹ năng văn phòng và định hướng nghề nghiệp để chuẩn bị đi làm.<br />
+            - Đối với các đối tượng đã đi làm: Bổ trợ về các kỹ năng tin học văn phòng, củng cố về các kiến thức bán hàng, marketing.
+          </p>
         </div>
       </div>
     </div>
   </div>
-
-  <div class="relative mt-80 md:mt-20 lg:mt-36 xl:mt-300">
-    <div class="absolute w-screen left-1/2 -translate-x-1/2 -translate-y-full bg-[#efefef] h-270 flex flex-col items-center">
-      <div class="w-full lg:w-225 xl:(w-300 h-250px) 2xl:(w-340 h-280px) mx-auto my-auto">
-        <div class="bg-white w-5/7 rd-1">
-          <div class="p10 space-y-5 -mt-70">
-            <div class="flex bg-#f2f7fd rd-2 sticky top-0">
-              <button
-                v-for="(tab, index) in tabs"
-                :key="index"
-                @click="changeTab(tab, index)"
-                :class="{
-                  'bg-color_4': activeTab === tab,
-                  'bg-#f2f7fd text-color_4': activeTab !== tab,
-                }"
-                class="px-4 py-2 rd-2 focus:outline-none text-xl"
-              >
-                {{ tab }}
-              </button>
-            </div>
-            <div class="text-3xl text-color_4 font-600">Tổng quan</div>
-            <div class="text-lg">
-              <p v-if="showFullText">{{ longText }}</p>
-              <p v-else>{{ shortenedText }}</p>
-              <button @click="toggleText" class="text-blue-500 bg-white text-lg">
-                {{ showFullText ? "Rút gọn" : "Xem thêm" }}
-              </button>
-            </div>
-            <div v-for="(tab, index) in tabs" :key="index" ref="tabElement" class="mt-10 bg-gray-200 p-4">
-              <p>Nội dung của Tab {{ index + 1 }}</p>
-            </div>
-
-            <div class="text-3xl text-color_4 font-600">DANH SÁCH</div>
-            <div>
-              <ul>
-                <li class="cursor-pointer" v-for="(item, index) in menuItems" :key="index">
-                  <div class="flex items-center justify-between text-base leading-6 bg-[#E3EFF9] p-5 text-xl" @click="toggleItem(item)">
-                    <div class="flex items-center">
-                      <div class="ml-2">
-                        <NuxtLink :to="item.url">
-                          {{ item.title }}
-                        </NuxtLink>
-                      </div>
-                    </div>
-                    <div v-if="item.children && item.children.length" :class="item.open ? 'i-mdi:chevron-up md:text-2xl' : 'i-mdi:chevron-down md:text-xl'"></div>
-                  </div>
-                  <div class="overflow-auto transition-all scrollbar-hide" :class="item.open && index === 0 ? 'h-32 duration-500' : 'h-0 duration-500'">
-                    <div v-for="(childItem, childIndex) in item.children" :key="childIndex">
-                      <div class="flex items-center text-base leading-6 p-5 ml-8">
-                        <div :class="item.icon"></div>
-                        <NuxtLink :to="childItem.url">
-                          {{ childItem.title }}
-                        </NuxtLink>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-            <div class="text-3xl text-color_4 font-600">ĐÁNH GIÁ</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="flex items-center text-3xl text-color_4 font-700 space-x-2 my-5">
+  <div class="flex items-center text-3xl text-color_4 font-700 space-x-2 my-5 w-full">
     <div class="i-mdi:cogs text-4xl"></div>
     <div>KHOÁ HỌC CÙNG LĨNH VỰC</div>
   </div>
-  <SlideCourse />
+  <div class="w-full">
+    <SlideCourse />
+  </div>
 </template>
 
 <script setup>
 import { ref } from "vue"
 import SlideCourse from "../../components/slide/SlideCourse.vue"
 // layouts
+const tabs = ["Tab1", "Tab2", "Tab3"]
+const data_tab = [
+  {
+    id: 1,
+    name: "Tổng quan",
+  },
+  {
+    id: 2,
+    name: "Danh sách",
+  },
+  {
+    id: 3,
+    name: "Đánh giá",
+  },
+]
 
-const tabs = ["Tab 1", "Tab 2", "Tab 3"]
-const activeTab = ref(tabs[0])
-const changeTab = tab => {
-  activeTab.value = tab
+const maxLength = 670
+const showFullText = ref(false)
+const tab_selected = ref()
+const shortenedText = computed(() => {
+  return showFullText.value ? longText : longText.slice(0, maxLength)
+})
+
+const activeTab = computed(() => {
+  console.log(tab_selected.value)
+  // const targetPosition = targetElement.getBoundingClientRect().top
+  // const currentPosition = window.innerHeight - offset
+
+  // if (targetPosition < currentPosition) {
+  //   console.log("Đã cuộn tới phần tử mong muốn")
+  //   // Thực hiện các hành động bạn muốn ở đây
+  // }
+  return
+})
+
+function changeTab(tab) {
+  console.log("🚀 ~ file: le.vue:115 ~ changeTab ~ tab:", tab)
+  tab_selected.value = tab.id
+  let x = ""
+  switch (tab.id) {
+    case 1:
+      const tab_1 = document.getElementById("1")
+      console.log("🚀 ~ file: le.vue:134 ~ changeTab ~ tab_1:", tab_1)
+      tab_1.scrollIntoView({ behavior: "smooth", block: "start", inline: "center" })
+      break
+    default:
+      break
+  }
   scrollToTab(tab)
 }
-
-const tabElement = ref([])
-const scrollToTab = tab => {
+function scrollToTab(tab) {
   const index = tabs.indexOf(tab)
   const tabRef = tabElement[index]
   if (tabRef) {
     tabRef.scrollIntoView({ behavior: "smooth", block: "start" })
   }
 }
-const maxLength = 670
-const showFullText = ref(false)
 
-const shortenedText = computed(() => {
-  return showFullText.value ? longText : longText.slice(0, maxLength)
-})
-
-const toggleText = () => {
+function toggleText() {
   showFullText.value = !showFullText.value
 }
 
