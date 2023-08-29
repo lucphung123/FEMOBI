c59e1638d2e05829034042678ba1221fe00ba5a6
Minh DT <duongtuanminhst@gmail.com>
2023-08-29 17:48:26 +0700

    update tabs

--- pages/test.vue
+++ pages/test.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="w-full">
-    <div class="flex text-center mt-10 bg-color_11 py-4 gap-0.5">
-      <div>
-        <a class="cursor-pointer p-5" :class="{ 'bg-color_4 text-white rounded-lg': selectedTab === 0 }" @click="selectTab(0)">Profile</a>
-      </div>
-      <div>
-        <a class="cursor-pointer p-5" :class="{ 'bg-color_4 text-white rounded-lg': selectedTab === 1 }" @click="selectTab(1)">Dashboard</a>
-      </div>
-      <div>
-        <a class="cursor-pointer p-5" :class="{ 'bg-color_4 text-white rounded-lg': selectedTab === 2 }" @click="selectTab(2)">Settings</a>
-      </div>
-      <div>
-        <a class="cursor-pointer p-5" :class="{ 'bg-color_4 text-white rounded-lg': selectedTab === 3 }" @click="selectTab(3)">Contacts</a>
-      </div>
+    <div class="min-h-screen flex justify-center items-center">
+      <input class="hidden" type="radio" v-for="(tab, index) in tabs" :key="index" :id="tab.id" :value="tab.id" v-model="activeTab" />
+      <nav class="w-[600px] h-[60px] flex text-center relative shadow-xl rounded-[5px] bg-white">
+        <div class="absolute h-full w-[20%] transition-all duration-400 ease-in-out z-10 rounded-[5px] left-0 top-0 bg-color_4" :style="{ left: sliderLeft }"></div>
+        <label class="w-full h-full leading-[60px] text-lg font-normal text-color_4 relative z-100 cursor-pointer transition-all duration-300 ease-in-out mx-[5px] my-0 rounded-[5px] hover:(bg-color_4 text-white)" v-for="(tab, index) in tabs" :key="index" :for="tab.id" :class="activeTab === tab.id ? '!text-white' : ''" @click="moveSlider(index)"> {{ tab.label }} </label>
+      </nav>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue"
-
-const selectedTab = ref(0)
-
-const selectTab = index => {
-  selectedTab.value = index
+const tabs = [
+  { id: "home", label: "Home" },
+  { id: "blog", label: "Blog" },
+  { id: "code", label: "Code" },
+  { id: "help", label: "Help" },
+  { id: "about", label: "About" },
+]
+const activeTab = ref("home")
+const sliderLeft = computed(() => {
+  const tabWidth = 100 / tabs.length
+  return tabWidth * tabs.findIndex(tab => tab.id === activeTab.value) + "%"
+})
+function moveSlider(index) {
+  activeTab.value = tabs[index].id
 }
 </script>
