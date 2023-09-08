abf894ec45ab83285dc81d42c6dba4a856079244
Minh DT <duongtuanminhst@gmail.com>
2023-09-08 10:24:42 +0700

    Update [slug]

--- components/Course/ImageTextSquare.vue
+++ components/Course/ImageTextSquare.vue
@@ -1,15 +1,14 @@
 <script setup>
 import { useCartStore } from "~~/stores/cartStore"
 const cartStore = useCartStore()
-const t = useNuxtApp().$i18n.t
 
 const router = useRouter()
 const route = useRoute()
 const router_push = (slug, id) => {
-  if (route.path === "/course/combo") {
-    return "/course/course_combo/" + slug
+  if (route.path === "/khoa-hoc/combo") {
+    return "/khoa-hoc/combo" + slug
   } else {
-    return "/course/detail-" + slug + "-" + id
+    return "/khoa-hoc/" + slug + "-" + id
   }
 }
 const props = defineProps({
@@ -29,26 +28,14 @@ const props = defineProps({
 })
 const addItemCart = item => {
   cartStore.addItemCart(item)
-  notification.success({
-    content: t('successfully-added-the-course-to-your-cart'),
-    duration: 1000,
-    keepAliveOnHover: true,
-  })
-
 }
 const removeItemCart = item => {
   cartStore.removeItemCart(item)
-  notification.success({
-    content: t('successfully-removed-course-from-cart'),
-    duration: 1000,
-    keepAliveOnHover: true,
-  })
-
 }
 </script>
 <template>
   <NuxtLink :to="router_push(props.item_course.slug, props.item_course.id)" class="bg-white rounded overflow-hidden shadow-md hover:(!shadow-lg !shadow-blue-400) cursor-pointer duration-300 flex flex-col">
-    <img class="w-full h-40 object-cover" preset="cover" :src="props.item_course.image_url !== '' ? props.item_course.image_url : '/empty.jpg'" placeholder="/empty.jpg" alt="image_course"/>
+    <img class="w-full h-40 object-cover" preset="cover" :src="props.item_course.image_url !== '' ? props.item_course.image_url : '/img/empty.jpg'" placeholder="/img/empty.jpg" alt="image_course" />
     <div class="p-2 flex flex-col flex-1">
       <div class="flex-1">
         <span class="font-semibold text-lg line-clamp-1">
@@ -56,17 +43,9 @@ const removeItemCart = item => {
         </span>
         <div>
           <div class="flex h-full items-center py-1">
-            <Icon name="ic:baseline-person-outline" class="text-xl" />
             <span>{{ props.item_course.owner.name }}</span>
           </div>
-          <!-- <div class="flex h-full items-center py-1">
-            <Icon name="mdi:folder-open-outline" class="text-xl"/>
-            <span>CEO World New Academy</span>
-          </div> -->
-          <div class="flex items-center py-1">
-            <n-rate size="small" readonly :default-value="props.item_course.review.star" />
-            <span class="px-1">({{ props.item_course.review.total }})</span>
-          </div>
+
           <div class="grid grid-cols-2">
             <span v-if="props.item_course.sale_price >= props.item_course.price"></span>
             <span v-else class="flex justify-start items-center line-through text-gray-400">
@@ -81,27 +60,6 @@ const removeItemCart = item => {
           </div>
         </div>
       </div>
-      <!-- <ClientOnly>
-        <n-button
-          v-if="
-            cartStore.cart.filter((item) => item.id == props.item_course.id)
-              .length == 0
-          "
-          type="primary"
-          class="w-full mt-2"
-          @click.prevent.stop="addItemCart(props.item_course)"
-        >
-          Thêm vào giỏ hàng
-        </n-button>
-        <n-button
-          v-else
-          type="info"
-          class="w-full mt-2"
-          @click.prevent.stop="removeItemCart(props.item_course)"
-        >
-          Đã Thêm Vào Giỏ Hàng
-        </n-button>
-      </ClientOnly> -->
     </div>
   </NuxtLink>
 </template>
--- pages/khoa-hoc/[slug].vue
+++ pages/khoa-hoc/[slug].vue
@@ -1,9 +1,62 @@
 <template>
-    <div>
-
+  <div class="w-full mx-auto container h-full xs:px-0 bg-white">
+    <div class="flex p-2">
+      <div class="w-full">
+        <div class="pl-3">
+          <div v-if="data.length > 0" class="grid grid-cols-1 sm:grid-cols-2 gap-6 md:(grid-cols-3) lg:grid-cols-4">
+            <CourseImageTextSquare class="bg-white rounded overflow-hidden shadow-md hover:(shadow-lg shadow-blue-400) cursor-pointer duration-300" v-for="(item, index) in data" :key="index" :item_course="item" />
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
+import { useSettingStore } from "~~/stores/settingStore"
+const settingStore = useSettingStore()
 
+const route = useRoute()
+const { RestApi } = useApi()
+
+const filter_value = ref({
+  time: null,
+  rating: null,
+  type: 1,
+  isFree: false,
+  isSale: false,
+})
+const pagination = ref({
+  page: 1,
+  limit: 27,
+  total: 0,
+})
+const renderLabelRate = option => {
+  return h(NRate, {
+    defaultValue: option.value,
+    readonly: true,
+    size: "small",
+  })
+}
+const query = computed(() => {
+  let query = {
+    page: pagination.value.page - 1,
+    limit: pagination.value.limit,
+    category: route.params.slug ? route.params.slug : "",
+    rating: filter_value.value.rating,
+    isFree: filter_value.value.isFree,
+    isSale: filter_value.value.isSale,
+    IdPosition: filter_value.value.type,
+  }
+  return query
+})
+const { data: courseData, pending: coursePending, error: courseError } = await RestApi.course.get({ query: query })
+let data = computed(() => {
+  if (courseData.value?.status) {
+    pagination.value.total = courseData.value.total
+    return courseData.value.item
+  } else {
+    return []
+  }
+})
 </script>
