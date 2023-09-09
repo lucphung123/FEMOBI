9dc6701682acec4a5a4815e2fb893d086ccc62e5
Minh DT <duongtuanminhst@gmail.com>
2023-09-09 14:57:19 +0700

    Update gio-hang

--- pages/gio-hang/index.vue
+++ pages/gio-hang/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-color_11 w-full">
-    <div v-if="courseItems.length === 0" class="bg-color_11 mt-10">
+    <div v-if="cartStore.cart.length === 0" class="bg-color_11 mt-10">
       <div class="items-center justify-center flex">
         <img src="/img/cart/shopping-cart.svg" alt="" />
       </div>
@@ -28,20 +28,22 @@
               <input type="checkbox" v-model="selectAll" @change="toggleSelectAll" />
               <p class="ml-2 text-color_6 text-16px">Sản phẩm ({{ selectedItems.length }} sản phẩm)</p>
             </div>
-            <p class="ml-52%">Giá</p>
+            <p class="ml-56%">Giá</p>
             <div></div>
           </div>
+
           <div class="bg-white rounded-lg p-5 mt-5">
-            <div v-for="(item, index) in courseItems" :key="item.id" class="flex border-b pb-5 mt-5">
+            <div v-for="course in cartStore.cart" :key="course.id" class="flex border-b pb-5 mt-5">
               <div class="gap-3 flex">
-                <input type="checkbox" v-model="selectedItems" :value="item" />
-                <img :src="item.image" alt="" />
+                <input type="checkbox" v-model="selectedItems" :value="course" />
+
+                <img class="w-140 h-40 object-cover" preset="cover" :src="course.image_url !== '' ? course.image_url : '/img/empty.jpg'" placeholder="/img/empty.jpg" alt="image_course" />
               </div>
-              <div class="flex ml-10 w-full">
-                <p class="text-color_8 text-4 leading-4.5 font-medium break-words w-120%">{{ item.name }}</p>
+              <div class="flex ml-10 justify-center items-center w-full">
+                <p class="text-color_8 text-4 font-bold leading-4.5 font-medium break-words w-120%">{{ course.title }}</p>
                 <div class="flex ml-20 justify-between w-full">
-                  <p class="text-color_4 font-bold text-4">{{ formatCurrency(item.price) }}</p>
-                  <div class="flex cursor-pointer" @click="deleteCourse(item.id)">
+                  <p class="text-color_4 font-bold text-4">{{ formatCurrency(course.sale_price) }}</p>
+                  <div class="flex cursor-pointer" @click="deleteCourse(course.id)">
                     <div class="i-mdi-trash text-2xl"></div>
                     <p class="ml-2">Xóa</p>
                   </div>
@@ -60,9 +62,9 @@
               <p class="text-color_9 font-normal text-sm">Tổng giá trị đơn hàng</p>
               <p class="text-color_4 font-bold text-4 mt-3">{{ formatCurrency(selectedTotalPrice) }}</p>
             </div>
-            <div class="mt-4">
-              <input type="text" placeholder="Mã giảm giá" class="h-7 border rounded-5 p-3 outline-none w-43" />
-              <button class="bg-color_4 text-white text-sm text-center h-7 rounded-5 ml-5 px-1 w-19">ÁP DỤNG</button>
+            <div class="mt-4 flex">
+              <input type="text" placeholder="Mã giảm giá" class="h-10 border rounded-5 p-3 outline-none w-70%" />
+              <button class="bg-color_4 text-white text-sm text-center h-10 rounded-5 ml-2 px-2 w-30%">ÁP DỤNG</button>
             </div>
             <nuxt-link to="/thanh-toan/thong-tin-thanh-toan">
               <button class="bg-color_1 text-white text-center font-bold leading-2.5 text-3.5 h-10 w-full mt-4 rounded-5">TIẾP TỤC THANH TOÁN</button>
@@ -86,38 +88,34 @@
 <script setup>
 import { ref, computed } from "vue"
 import SlideCourse from "../../components/slide/SlideCourse.vue"
+import { useCartStore } from "~~/stores/cartStore"
+const cartStore = useCartStore()
 // layouts
 definePageMeta({
   layout: "course",
 })
-
-const courseItems = ref([
-  { id: 1, name: "108 Tọa pháp Yoga - Bí mật trẻ mãi", price: 700000, image: "/img/course1/Tieng-anh.png" },
-  { id: 2, name: "Nhập môn cờ vua cho người mới bắt đầu", price: 900000, image: "/img/course1/suc-khoe.png" },
-  { id: 3, name: "Nhập môn cờ vua cho người mới bắt đầu", price: 500000, image: "/img/course1/Thpt.png" },
-])
 
 const selectedItems = ref([])
 const selectAll = ref(false)
 
 const toggleSelectAll = () => {
   if (selectAll.value) {
-    selectedItems.value = [...courseItems.value]
+    selectedItems.value = [...cartStore.cart]
   } else {
     selectedItems.value = []
   }
 }
 
 function deleteCourse(id) {
-  const index = courseItems.value.findIndex(item => item.id === id)
+  const index = cartStore.cart.findIndex(course => course.id === id)
   if (index !== -1) {
-    courseItems.value.splice(index, 1)
+    cartStore.cart.splice(index, 1)
   }
 }
 function formatCurrency(value) {
   return new Intl.NumberFormat("vi-VN", { style: "currency", currency: "VND" }).format(value)
 }
 const selectedTotalPrice = computed(() => {
-  return selectedItems.value.reduce((total, item) => total + item.price, 0)
+  return selectedItems.value.reduce((total, course) => total + course.sale_price, 0)
 })
 </script>
