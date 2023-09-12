0d8b4ec2a1e419cba4b8b4a17e97938ca341e6a8
thanhnt <thanhnt2@inet.vn>
2023-09-12 11:47:13 +0700

    init

--- pages/test.vue
+++ pages/test.vue
@@ -1,34 +1,33 @@
 <template>
-  <div>
-    <div>
-      <div class="flex justify-between items-center">
-        <button class="bg-gray-700 text-white w-full text-left outline-none text-base hover:bg-gray-600" @click="toggle(0)">Open Collapsible</button>
-        <div :class="arrowIcon(0)"></div>
-      </div>
-      <div class="leading-50" :class="contentClasses(0)">
-        <p>Lorem ipsum dolor sit amet...</p>
-        <p>Another line of content...</p>
-        <p>Yet another line of content...</p>
+  <div class="w-full h-full flex flex-col items-center mt-10">
+    <div class="relative w-full h-12">
+      <div class="absolute h-full transition-all duration-400 ease-in-out z-4 rounded-[5px] left-0 top-0 bg-color_4" :style="{ width: sliderWidth, left: sliderLeft }"></div>
+      <div v-for="(tab, index) in tabs" :key="index" class="inline-block w-1/3 h-full text-center leading-12 text-black cursor-pointer transition-all duration-300 ease-in-out rounded hover:(bg-color_4 text-white)" @click="moveSlider($event, index)">
+        {{ tab.label }}
       </div>
     </div>
   </div>
 </template>
-
 <script setup>
-const activeSection = ref(-1)
-const toggle = sectionIndex => {
-  if (activeSection.value === sectionIndex) {
-    activeSection.value = -1
-  } else {
-    activeSection.value = sectionIndex
-  }
-}
-const contentClasses = sectionIndex => {
-  const baseClasses = "overflow-hidden transition-max-height ease-out duration-800 bg-gray-200"
-  const activeClasses = activeSection.value === sectionIndex ? "max-h-200" : "max-h-0"
-  return `${baseClasses} ${activeClasses}`
-}
-const arrowIcon = sectionIndex => {
-  return activeSection.value === sectionIndex ? "i-mdi:chevron-up md:text-xl" : "i-mdi:chevron-down md:text-xl"
+definePageMeta({
+  layout: "",
+})
+const tabs = [
+  { id: 1, label: "Tab 1sdddddddddddddddddddddddddddddddddddddddddddddddddddd" },
+  { id: 2, label: "Tab 2" },
+  { id: 3, label: "Tab 3" },
+]
+
+const activeTab = ref(1)
+const sliderWidth = ref("")
+const sliderLeft = ref("0%")
+function moveSlider(event, index) {
+  activeTab.value = tabs[index].id
+  // Lấy chiều rộng của tab và cập nhật sliderWidth
+  const tabWidth = event.currentTarget.offsetWidth
+  sliderWidth.value = `${tabWidth}px`
+  // Cập nhật vị trí của slider
+  const tabLeft = event.currentTarget.offsetLeft
+  sliderLeft.value = `${tabLeft}px`
 }
 </script>
